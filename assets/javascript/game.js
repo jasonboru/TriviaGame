2cf188041f36efd03cfb4eb443891e059afa70b9
Jason OBrien <Jason OBrien>
2017-02-25 20:44:50 -0500

    initial file setup. added some questions and sounds.

---
+++ assets/javascript/game.js
@@ -0,0 +1,139 @@
+
+$(document).ready(function() {
+
+var questions = [
+    {
+        ques: "This writer is acreditied for writing the first Christian epic poem, \"Paradise Lost\".",
+        answ: "Milton",
+        sound: "../assets/sounds/milton.mp3"
+    },
+
+    {
+        ques: "This renowned physist came to the United States seeking political assylu.",
+        answ: "Albert Einstein",
+        sound: "../assets/sounds/einstein.mp3"
+    },
+
+    {
+        ques: "He was known as \“the Great Dissenter\” for his principled stands during three decades on the U.S. Supreme Court.",
+        answ: "Oliver Wendell Holmes",
+        sound: "../assets/sounds/holmes.mp3"
+    },
+
+    {
+        ques: "He was an American mythologist, writer and lecturer, best known for his work in comparative mythology and comparative religion",
+        answ: "Joseph Campbell",
+        sound: "../assets/sounds/campbell.mp3"
+    },
+
+        {
+        ques: "This ancient Greek philosopher was tutor to Alexander the Great",
+        answ: "Aristotle",
+        sound: "../assets/sounds/aristotle.mp3"
+    },
+
+    {
+        ques: "In 1939 he designed and flew the first viable American helicopter, which pioneered the rotor configuration used by most helicopters today",
+        answ: "Igor Sikorsky",
+        sound: "../assets/sounds/sikorsky.mp3"
+    },
+
+    {
+        ques: "After his invention of Dynamite he made a fortune, but feared being remembered as war profiteer.",
+        answ: "Alfred Nobel",
+        sound: "../assets/sounds/nobel.mp3"
+    },
+
+    {
+        ques: "Literaly having a hand in writing the First Admendment this President of the United States asserted that the liberty of the nation depends on the freedom of the press without limitation.",
+        answ: "Thomas Jefferson",
+        sound: "../assets/sounds/jefferson.mp3"
+    },
+
+    {
+        ques: "Ancient Roman poet of the Augustan period. He wrote three of the most famous poems in Latin literature, the Eclogues, the Georgics, and the epic Aeneid.",
+        answ: "Virgil",
+        sound: "../assets/sounds/virgil.mp3"
+    },
+
+    {
+        ques: "During World War II, especially during the Blitz, he achieved great celebrity status as a result of his war reports. His catchphrase was \"good night, and good luck.\"",
+        answ: "Edward R. Murrow",
+        sound: "../assets/sounds/murrow.mp3"
+    },
+
+    {
+        ques: "This English mathematician, astronomer, and physicist is widely recognised as one of the most influential scientists of all time and a key figure in the scientific revolution.",
+        answ: "Issac Newton",
+        sound: "../assets/sounds/newton.mp3"
+    },
+
+    {
+        ques: "His influence on the Latin language was so immense that the subsequent history of prose in not only Latin but European languages up to the 19th century was said to be either a reaction against or a return to his style.",
+        answ: "Cicero",
+        sound: "../assets/sounds/cicero.mp3"
+    },
+
+    {
+        ques: "For proving that the Sun, not the Earth, was the center of our solar system this astronomer was sentence to house arrest for the remainder of his life.",
+        answ: "Galileo",
+        sound: "../assets/sounds/galileo.mp3"
+    },
+
+    {
+        ques: "This American industrialist was the largest sponsor of the development of the assembly line technique of mass production.",
+        answ: "Henry Ford",
+        sound: "../assets/sounds/ford.mp3"
+    },
+
+    {
+        ques: "This Author was vice president of Mensa International, albeit reluctantly; he described some members of that organization as \"brain-proud and aggressive about their IQs\". He took more joy in being president of the American Humanist Association.",
+        answ: "Isaac Asimov",
+        sound: "../assets/sounds/asimov.mp3"
+    },
+
+    {
+        ques: "This American poet and Georgist from New York City, is best known for \"The New Colossus\", a sonnet written in 1883.",
+        answ: "Emma Lazarus",
+        sound: "../assets/sounds/lazarus.mp3"
+    },
+
+    {
+        ques: "Author of The Art of War, a widely influential work of military strategy that has affected both Western and Eastern philosophy.",
+        answ: "Sun Tzu",
+        sound: "../assets/sounds/suntzu.mp3"
+    },
+
+    {
+        ques: "His name has been leant to a negative term used to characterize unscrupulous politicians of the sort described most famously in his work \"The Prince\".",
+        answ: "Niccolo Machiavelli",
+        sound: "../assets/sounds/machiavelli.mp3"
+    },
+
+    {
+        ques: "After becoming a successful writer, this great American Satirist sunk his money into a number of bad investments and eventually went bankrupt.",
+        answ: "Mark Twian",
+        sound: "../assets/sounds/twain.mp3"
+    },
+
+    {
+        ques: "An Athenian politician and general, he was one of a new breed of non-aristocratic politicians who rose to prominence in the early years of democracy.",
+        answ: "Themistocles",
+        sound: "../assets/sounds/themistocles.mp3"
+    },
+
+    {
+        ques: "Called the \"father of the atomic bomb\" for his role in the Manhattan Project, the World War II project that developed the first nuclear weapons",
+        answ: "Robert Oppenheimer",
+        sound: "../assets/sounds/oppenheimer.mp3"
+    },
+
+
+
+
+
+
+]
+
+    
+});
